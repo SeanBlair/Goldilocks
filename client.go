2b679af316aef52dba2cd6d07754d7fde1175cf9
Sean Blair <seandibango@gmail.com>
2017-01-12 22:46:26 -0800

    Add documentation comments

--- client.go
+++ client.go
@@ -38,26 +38,25 @@ func main() {
 
 	LocalIpAndPort, err := net.ResolveUDPAddr("udp", local_ip_port)
 	CheckError(err)
-
 	ServerIpAndPort, err := net.ResolveUDPAddr("udp", remote_ip_port)
 	CheckError(err)
 
 	var minimum uint32 = 0
 	var maximum uint32 = math.MaxUint32
 
-	// Finds the "fortune" using binary search
+	// Find the "fortune" using binary search
 	for {
 		var guess uint32 = ComputeGuess(minimum, maximum)
 		buf, err := Marshall(guess)
 		CheckError(err)
 
-		// send guess to server 
+		// Send guess to server
 		// if timeout, resend until receive response
 		// else compute next guess or print result
 		isTimeout := true
 
 		for isTimeout {
-
+			// send guess
 			Conn, err := net.DialUDP("udp", LocalIpAndPort, ServerIpAndPort)
 			CheckError(err)
 			_, err = Conn.Write(buf)
@@ -81,11 +80,10 @@ func main() {
 
 			} else {
 				isTimeout = false
-
 				ServerConn.Close()
 
 				// n = length of response
-				// 4 == "high", 3 == "low", else variable sized "fortune"
+				// 4 == "high", 3 == "low", else variable length "fortune"
 				switch n {
 				case 4:
 					maximum = guess
@@ -107,7 +105,7 @@ func Marshall(guess uint32) ([]byte, error) {
 	return network.Bytes(), err
 }
 
-// Prints error message and exits program
+// If error exists, prints error message and exits program
 func CheckError(err error) {
 	if err != nil {
 		log.Fatal("ERROR: ", err)
